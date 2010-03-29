eefd269510c300e355cda3e754e02b0adf66886d
andra.waagmeester <andra.waagmeester@781e20b4-2c82-11df-bd14-c1e331e6ceac>
2010-03-29 13:13:59 +0000

    Added new colorscheme/
    git-svn-id: http://citedin.googlecode.com/svn/trunk@9 781e20b4-2c82-11df-bd14-c1e331e6ceac

--- index_ui4.php
+++ index_ui4.php
@@ -6,7 +6,7 @@
 		</title>
 		
 		<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.4.1/jquery.min.js"></script>
-		<link type="text/css" href="development-bundle/themes/base/ui.all.css" rel="stylesheet">
+		<link type="text/css" href="development-bundle/themes/custom-theme/ui.all.css" rel="stylesheet">
 		<script type="text/javascript" src="highslide/highslide-with-html.js"></script>
 		<link rel="stylesheet" type="text/css" href="highslide/highslide.css" />
 		<script type="text/javascript" src="development-bundle/ui/ui.core.js"></script>
@@ -74,7 +74,7 @@
 				</li>
 			</ul>
 			<div id="fragment-1">
-
+				Citedin finds where you are cited! Some of your papers may have been mentioned where you didn’t expect that to happen: in blogs, databases, Wikipedia. Citedin finds them all.
 				<?php
 				$resources = scandir('resources/');
 				$citedin_resources = array();
@@ -103,11 +103,11 @@
 					print "<center>
 								<div id=\"rowSearch\">
 									
-								<div id=\"SearchBox\"><div class=\"SearchHeader\">Publication search</div><div class=\"box_text\">Through this website you can track various resources citing a PubMed Identifier. To find a pubmed identifier use this search form.<br>
+								<div id=\"SearchBox\"><div class=\"ui-widget-header\">Publication search</div><div class=\"box_text\">Through this website you can track various resources citing a PubMed Identifier. To find a pubmed identifier use this search form.<br>
 								<P style=\"TEXT-ALIGN: right\"><SPAN style=\"FONT-SIZE: x-small\">(e.g. 
 									<a style=\"cursor:pointer;text-decoration: underline;\" onclick=\"document.getElementById('pmidQuery').value='Waagmeester';\">Waagmeester</a>, 
 									<a style=\"cursor:pointer;text-decoration: underline;\" onclick=\"document.getElementById('pmidQuery').value='Kelder';\">Kelder</a>, <a style=\"cursor:pointer;text-decoration: underline;\" onclick=\"document.getElementById('pmidQuery').value='Evelo';\">Evelo</a>, or <a style=\"cursor:pointer;text-decoration: underline;\" onclick=\"document.getElementById('pmidQuery').value='WikiPathways';\">WikiPathways</a>)</SPAN></P><input name=\"pmidQuery\" id=\"pmidQuery\" type=\"text\" /><input type=\"submit\" id=\"citedinQuery\" value=\"Submit\" /></div></div>
-								<div id=\"SearchBox\"><div class=\"SearchHeader\">Resources citing a pubmed ID</div><div class=\"box_text\"> Submit a PubMed Identifier to find resources citing this abstract. <P style=\"TEXT-ALIGN: right\"><SPAN style=\"FONT-SIZE: x-small\">(e.g. 
+								<div id=\"SearchBox\"><div class=\"ui-widget-header\">Resources citing a pubmed ID</div><div class=\"box_text\"> Submit a PubMed Identifier to find resources citing this abstract. <P style=\"TEXT-ALIGN: right\"><SPAN style=\"FONT-SIZE: x-small\">(e.g. 
 									<a style=\"cursor:pointer;text-decoration: underline;\" onclick=\"document.getElementById('pmid').value='15489334';\">15489334</a>, <a style=\"cursor:pointer;text-decoration: underline;\" onclick=\"document.getElementById('pmid').value='18651794';\">18651794</a>)</SPAN></P><input name=\"pmid\" id=\"pmid\" type=\"text\" value=\"$pmidGet\" /><input type=\"submit\" id=\"citedin\" value=\"Submit\" /></div></div></div></center>";
 
 	
@@ -121,7 +121,7 @@
 													
 													$(\"#citedin\").click(function(){\n
 															var pmid = $(\"input#pmid\").val();\n"; 
-															print 		"$(\"#pubmedDetails\").load(\"resources/getPubmed.php?pmid=\"+pmid);\n";
+//															print 		"$(\"#pubmedDetails\").load(\"resources/getPubmed.php?pmid=\"+pmid);\n";
 															foreach ($citedin_resources as $div){
 																print "$(\"#$div\").load(\"resources/$div?pmid=\"+pmid, CitedIn.afterResourceLoad);\n";
 															}
@@ -152,7 +152,12 @@
 				<?php include("included_resources.php"); ?>
 			</div>
 			<div id="fragment-3">
-				If you know a resource that would enrich this website please send us an <a href="mailto:andra.waagmeester@bigcat.unimaas.nl">email</a>
+				If you know a resource that would enrich this website please send us an <a href="mailto:andra.waagmeester@bigcat.unimaas.nl">email</a><p>
+ 				Three types of resources are fit to be included in citedin.org, these are: 
+ 				<ul>
+					<li><b>Resources with webservices or an Application Programming Interface (API)</b>, which have functionality to search for pubmed identifiers. Examples with an API, are Wikipedia, Google Books, Google Blog and Connotea.</li>
+					<li><b>Complete databases dumps.</b> Most of the resources in citedin.org are obtained through parsing online available database dumps.</li>
+					<li><b>Supplementary data to published papers</b>. Some peer reviewed journal publication contain, next to the genereal references, appendices with (large) sets of pubmed identifiers. These citations, which are available as supplementary data are usually not considered as part of the papers bibliography. </li>
 			</div>
 			<div id="fragment-4">
 				<?php include("about_citedin.html"); ?>
