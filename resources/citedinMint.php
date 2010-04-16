c28f6e8e59df0b2b48c4d0b6bfd9ceb5a82ad3fb
andra.waagmeester <andra.waagmeester@781e20b4-2c82-11df-bd14-c1e331e6ceac>
2010-04-16 21:12:51 +0000

    Add mint as a resource to citedin
    git-svn-id: http://citedin.googlecode.com/svn/trunk@19 781e20b4-2c82-11df-bd14-c1e331e6ceac

---
+++ resources/citedinMint.php
@@ -0,0 +1,28 @@
+<?php
+require_once("ResourceRegistry.php");
+
+class MintResource implements Resource {
+	function getResourceName() { return "MINT, the Molecular INTeraction database"; }
+	function getInfoLink() { return "http://mint.bio.uniroma2.it/mint/Welcome.do"; }
+	function getResourceType() { return "Database"; }
+	function getResourceDescription() { return ""; }
+	function getResourceFilename() { return basename(__FILE__); }
+
+	public function getData($pmid) {
+	   include 'connectdb.inc';
+	   $result = mysql_query("SELECT * from kegg where pmid=$pmid");
+	   $num_rows = mysql_num_rows($result);
+
+		$data = new ResourceData();
+		$data->setCiteCount($num_rows)
+				->setResourceName($this->getResourceName())
+				->setInfoLink($this->getInfoLink())
+				->setDetailsLink("http://mint.bio.uniroma2.it/mint/search/publication.do?pmid=$pmid"); 
+
+		return $data;
+	}
+}
+
+ResourceRegistry::register("Mint", new MintResource());
+
+?>
