d0d295865d522af348143fb209f160d555d93bb5
andra.waagmeester@gmail.com <andra.waagmeester@gmail.com@781e20b4-2c82-11df-bd14-c1e331e6ceac>
2010-04-17 08:20:54 +0000

    included_resources -3
    git-svn-id: http://citedin.googlecode.com/svn/trunk@24 781e20b4-2c82-11df-bd14-c1e331e6ceac

--- included_resources.php
+++ included_resources.php
@@ -3,7 +3,7 @@ require_once("resources/ResourceRegistry.php");
 ResourceRegistry::init();
 $citedin_resources = ResourceRegistry::listResources();
 foreach ($citedin_resources as $resource) {
-	$resource_info = ResourceRegistry::get($resource);
+	$resourceInfo = ResourceRegistry::get($resource);
 	var_dump($resourceInfo->getResourceName());
 }
 
