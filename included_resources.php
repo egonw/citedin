ffb3fb222eb03dfcee343f9a4db09caf73e6d431
andra.waagmeester@gmail.com <andra.waagmeester@gmail.com@781e20b4-2c82-11df-bd14-c1e331e6ceac>
2010-04-17 08:17:55 +0000

    included_resources added
    git-svn-id: http://citedin.googlecode.com/svn/trunk@22 781e20b4-2c82-11df-bd14-c1e331e6ceac

--- included_resources.php
+++ included_resources.php
@@ -1,20 +1,9 @@
 <?php
-   include ('resources/connectdb.inc');
-   $resources = scandir('data_files/');
-   print "<table><tr><th>Name</th><th>organisation</th><th>last update</th><th>file name</th><th>Update frequency</th></tr>\n";
-   foreach ($resources as $resource){
-      if (($resource != ".") and ($resource != ".."))
-       {
-       $sql = "select * from resources where filename = \"$resource\"";
-       $result = mysql_query($sql);
-       while ($row = mysql_fetch_assoc($result)) {
-	print "<tr><td>".$row["name"]."</td>";
-        print "<td><a href=\"".$row["orgurl"]."\">".$row["organisation"]."</a></td>";
-	print "<td> " . date("F d Y", filectime("data_files/$resource"))."</td>";
-        print "<td>$resource [<a href=\"".$row["url"]."\">get</a>]</td>";
-        print "<td>".$row["updatefreq"].".</tr>\n";
-       }
-       }
-   }
-   print "</table>";
+require_once("resources/ResourceRegistry.php");
+ResourceRegistry::init();
+$citedin_resources = ResourceRegistry::listResources();
+foreach ($citedin_resources as $resource) {
+	var_dump(ResourceRegistry::get($resource));
+}
+
 ?>
