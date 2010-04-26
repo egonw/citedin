359ad8ebdeb3fde307f37e2769897d926656ce60
andra.waagmeester@gmail.com <andra.waagmeester@gmail.com@781e20b4-2c82-11df-bd14-c1e331e6ceac>
2010-04-26 12:53:09 +0000

    Fixed submit problem, now queries can be submitted through click and return
    git-svn-id: http://citedin.googlecode.com/svn/trunk@33 781e20b4-2c82-11df-bd14-c1e331e6ceac

--- fragment_1.php
+++ fragment_1.php
@@ -6,17 +6,6 @@ ResourceRegistry::init();
 $url = "http://".$_SERVER["HTTP_HOST"].dirname($_SERVER["PHP_SELF"]);
 print $url;
 $citedin_resources = ResourceRegistry::listResources();
-/*foreach ($citedin_resources as $resource) {
-	$resourceInfo = ResourceRegistry::get($resource);
-	$fileName = $resourceInfo->getResourceName();
-	$className = $resourceInfo->getResourceClassname();
-	$fileName = basename($resourceInfo->getResourceFilename());	
-	print $resource."<BR>";
-	//print file_get_contents("http://localhost/citedin/resources/getHTML.php?pmid=".$_GET["pmid"]."&script=$fileName&class=$className");
-	break;
-	
-}*/
-// var_dump($citedin_resources);				
 if (isset($_GET["pmid"])){
 	$pmidGet = $_GET["pmid"];
 	print $url;
@@ -42,9 +31,12 @@ print "<center>
 	<div id=\"SearchBox\"><div class=\"ui-widget-header\">Publication search</div><div class=\"box_text\">Through this website you can track various resources citing a PubMed Identifier. To find a pubmed identifier use this search form.<br>
 <P style=\"TEXT-ALIGN: right\"><SPAN style=\"FONT-SIZE: x-small\">(e.g. 
 	<a style=\"cursor:pointer;text-decoration: underline;\" onclick=\"document.getElementById('pmidQuery').value='Waagmeester';\">Waagmeester</a>, 
-	<a style=\"cursor:pointer;text-decoration: underline;\" onclick=\"document.getElementById('pmidQuery').value='Kelder';\">Kelder</a>, <a style=\"cursor:pointer;text-decoration: underline;\" onclick=\"document.getElementById('pmidQuery').value='Evelo';\">Evelo</a>, or <a style=\"cursor:pointer;text-decoration: underline;\" onclick=\"document.getElementById('pmidQuery').value='WikiPathways';\">WikiPathways</a>)</SPAN></P><input name=\"pmidQuery\" id=\"pmidQuery\" type=\"text\" /><input type=\"submit\" id=\"citedinQuery\" value=\"Submit\" /></div></div>
+	<a style=\"cursor:pointer;text-decoration: underline;\" onclick=\"document.getElementById('pmidQuery').value='Kelder';\">Kelder</a>, <a style=\"cursor:pointer;text-decoration: underline;\" onclick=\"document.getElementById('pmidQuery').value='Evelo';\">Evelo</a>, or <a style=\"cursor:pointer;text-decoration: underline;\" onclick=\"document.getElementById('pmidQuery').value='WikiPathways';\">WikiPathways</a>)</SPAN></P>
+	<form id=\"queryForm\"><input name=\"pmidQuery\" id=\"pmidQuery\" type=\"text\" /><input type=\"submit\" id=\"citedinQuery\" value=\"Submit\" /></form></div></div>
+	
+	
 <div id=\"SearchBox\"><div class=\"ui-widget-header\">Resources citing a pubmed ID</div><div class=\"box_text\"> Submit a PubMed Identifier to find resources citing this abstract. <P style=\"TEXT-ALIGN: right\"><SPAN style=\"FONT-SIZE: x-small\">(e.g. 
-	<a style=\"cursor:pointer;text-decoration: underline;\" onclick=\"document.getElementById('pmid').value='15489334';\">15489334</a>, <a style=\"cursor:pointer;text-decoration: underline;\" onclick=\"document.getElementById('pmid').value='18651794';\">18651794</a>)</SPAN></P><input name=\"pmid\" id=\"pmid\" type=\"text\" value=\"$pmidGet\" /><input type=\"submit\" id=\"citedin\" value=\"Submit\" /></div></div></div></center>";
+	<a style=\"cursor:pointer;text-decoration: underline;\" onclick=\"document.getElementById('pmid').value='15489334';\">15489334</a>, <a style=\"cursor:pointer;text-decoration: underline;\" onclick=\"document.getElementById('pmid').value='18651794';\">18651794</a>)</SPAN></P><form id=\"citedinForm\"><input name=\"pmid\" id=\"pmid\" type=\"text\" value=\"$pmidGet\" /><input type=\"submit\" id=\"citedin\" value=\"Submit\" /></form></div></div></div></center>";
 
 
 
@@ -55,7 +47,10 @@ print "<script type=\"text/javascript\">\n
 	$(document).ajaxStop($.unblockUI);
 $(document).ready(function(){\n
 
-$(\"#citedin\").click(function(){\n
+
+
+$(\"#citedinForm\").submit(function(){\n
+
 	var pmid = $(\"input#pmid\").val();\n"; 
 	//															print 		"$(\"#pubmedDetails\").load(\"resources/getPubmed.php?pmid=\"+pmid);\n";
 	foreach ($citedin_resources as $resource){
@@ -66,7 +61,8 @@ $(\"#citedin\").click(function(){\n
 	}
 	print "});\n
 
-	$(\"#citedinQuery\").click(function(){
+		$(\"#queryForm\").submit(function(){\n
+			event.preventDefault();
 		$.blockUI({ message: '<h1><img src=\"pix/wait.gif\" /> Loading data ...</h1>' });	
 		var query = $(\"input#pmidQuery\").val();\n";
 
