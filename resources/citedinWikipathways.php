5c1be9c02a731b34ef69771fd46c415cf03ede7e
andra.waagmeester@gmail.com <andra.waagmeester@gmail.com@781e20b4-2c82-11df-bd14-c1e331e6ceac>
2010-04-26 16:42:25 +0000

    Made wikipathways available through its API
    git-svn-id: http://citedin.googlecode.com/svn/trunk@35 781e20b4-2c82-11df-bd14-c1e331e6ceac

--- resources/citedinWikipathways.php
+++ resources/citedinWikipathways.php
@@ -4,14 +4,17 @@ require_once("ResourceRegistry.php");
 class WikipathwaysResource implements Resource {
 	function getResourceName() { return "WikiPathways"; }
 	function getInfoLink() { return "http://www.wikipathways.org"; }
-	function getResourceType() { return "Database"; }
+	function getResourceType() { return "API"; }
 	function getResourceDescription() { return "In the new tradition of Wikipedia, WikiPathways is an open, public platform dedicated to the curation of biological pathways by and for the scientific community."; }
 	function getResourceFilename() { return basename(__FILE__); }
 
 	public function getData($pmid) {
-	   include 'connectdb.inc';
-	   $result = mysql_query("SELECT * from wikipathways where pmid=$pmid");
-	   $num_rows = mysql_num_rows($result);
+		
+		$client = new
+		SoapClient('http://www.wikipathways.org/wpi/webservice/webservice.php?wsdl');
+	    $results = $client->findPathwaysByLiterature(array('query'=>$pmid));
+	   
+		    $num_rows = count($results->result);
 
 		$data = new ResourceData();
 		$data->setCiteCount($num_rows)
@@ -26,3 +29,6 @@ class WikipathwaysResource implements Resource {
 ResourceRegistry::register("Wikipathways", new WikipathwaysResource());
   
 ?>
+	
+
+	
