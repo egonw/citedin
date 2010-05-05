b7315fc32bcf331eb7056b11fdc25b1a849e5323
andra.waagmeester <andra.waagmeester@781e20b4-2c82-11df-bd14-c1e331e6ceac>
2010-05-05 17:13:48 +0000

    Added Mouse Genome Informatics
    git-svn-id: http://citedin.googlecode.com/svn/trunk@49 781e20b4-2c82-11df-bd14-c1e331e6ceac

--- resources/citedinMgi.php
+++ resources/citedinMgi.php
@@ -2,27 +2,27 @@
 
 require_once("ResourceRegistry.php");
 
-class HmdbResource implements Resource {
+class MgiResource implements Resource {
 	function getResourceName() { return "Hmdb: Human Metabolome DataBase"; }
 	function getInfoLink() { return "http://www.hmdb.ca/"; }
 	function getResourceType() { return "Database"; }
-	function getResourceDescription() { return "The Human Metabolome Database (HMDB) is a freely available electronic database containing detailed information about small molecule metabolites found in the human body."; }
+	function getResourceDescription() { return "MGI is the international database resource for the laboratory mouse, providing integrated genetic, genomic, and biological data to facilitate the study of human health and disease."; }
 	function getResourceFilename() { return basename(__FILE__); }
 
 	public function getData($pmid) {
 	   include 'connectdb.inc';
-	   $result = mysql_query("SELECT * from hmdb where pmid=$pmid");
+	   $result = mysql_query("SELECT * from mgidb where pmid=$pmid");
 	   $num_rows = mysql_num_rows($result);
 
 		$data = new ResourceData();
 		$data->setCiteCount($num_rows)
 		     ->setResourceName($this->getResourceName())
-		     ->setDetailsLink("details.php?db=hmdb&pmid=$pmid&fields=hmdbid,url&idField=hmdbid"); 
+		     ->setDetailsLink("details.php?db=mgidb&pmid=$pmid&fields=mgiid,url&idField=mgiid"); 
 	     
 		return $data;
 	}
 }
 
-ResourceRegistry::register("Hmdb", new HmdbResource());
+ResourceRegistry::register("Mgi", new MgiResource());
 
 ?>
