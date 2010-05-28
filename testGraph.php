2511363be30b5001407e7e1a6e8f767b60e99f10
andra.waagmeester@gmail.com <andra.waagmeester@gmail.com@781e20b4-2c82-11df-bd14-c1e331e6ceac>
2010-05-28 07:43:38 +0000

    minor update
    git-svn-id: http://citedin.googlecode.com/svn/trunk@111 781e20b4-2c82-11df-bd14-c1e331e6ceac

--- testGraph.php
+++ testGraph.php
@@ -11,8 +11,6 @@ foreach ($citedin_resources as $resource){
 	print "<th scope=\"col\">$resource</th>";
 }
 $pmid = "15489334";
-print "</tr></thead><tbody><tr><th scope=\"row\"></th>";
-print "<tr><th scope=\"row\">$pmid</th>";
 $datapoints = array();
 $datanames = array();
 $max=0;
@@ -25,12 +23,10 @@ foreach ($citedin_resources as $resource) {
 	if ($data->getCiteCount() > $max) $max = $data->getCiteCount();
 	 array_push($datanames, $resource." (".$data->getCiteCount().")");
 	}
-     print "<td>".$data->getCiteCount()."</td>";    	}
+	   	}
 }
 
-print "</tr></table>";
 if ($max >100) $range = "&chds=0,$max";
 $urlimage = "http://chart.apis.google.com/chart?cht=p&chs=512x214$range&chtt=Citation+distribution&chd=t:".trim(implode(",", $datapoints))."&chl=".implode("|",$datanames);
 print "<img src = \"$urlimage\">";
-print $urlimage;
 ?>
