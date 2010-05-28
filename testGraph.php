31542ddedd97afb491023645d977423ce7c64b96
andra.waagmeester@gmail.com <andra.waagmeester@gmail.com@781e20b4-2c82-11df-bd14-c1e331e6ceac>
2010-05-28 10:07:27 +0000

    minor update
    git-svn-id: http://citedin.googlecode.com/svn/trunk@114 781e20b4-2c82-11df-bd14-c1e331e6ceac

--- testGraph.php
+++ testGraph.php
@@ -10,17 +10,17 @@ $datanames = array();
 $max=0;
 foreach ($citedin_resources as $resource) {
 	$resourceObject = ResourceRegistry::get($resource);
-	if ($resourceObject->getResourceType() == "Database"){
+	
 	$data = $resourceObject->getData($pmid);
 	if (($data->getCiteCount()>0) AND (is_int($data->getCiteCount()))) {
 	 array_push($datapoints, $data->getCiteCount());
 	if ($data->getCiteCount() > $max) $max = $data->getCiteCount();
 	 array_push($datanames, $resource." (".$data->getCiteCount().")");
-	}
+	
 	   	}
 }
 
-if ($max >100) $range = "&chds=0,$max";
+if ($max >100) $range = "";
 $urlimage = "http://chart.apis.google.com/chart?cht=p&chs=512x214$range&chtt=Citation+distribution&chd=t:".trim(implode(",", $datapoints))."&chl=".implode("|",$datanames);
 print "<img src = \"$urlimage\">";
 ?>
