d3f93e05d2151a1b8e12be8cc1c25bde925361db
andra.waagmeester@gmail.com <andra.waagmeester@gmail.com@781e20b4-2c82-11df-bd14-c1e331e6ceac>
2010-05-28 07:38:51 +0000

    minor update
    git-svn-id: http://citedin.googlecode.com/svn/trunk@109 781e20b4-2c82-11df-bd14-c1e331e6ceac

--- testGraph.php
+++ testGraph.php
@@ -18,17 +18,20 @@ $datanames = array();
 $max=0;
 foreach ($citedin_resources as $resource) {
 	$resourceObject = ResourceRegistry::get($resource);
+	if ($resourceObject->getResourceType() == "Database"){
+		print "<li>".$resourceObject->getResourceType();
 	$data = $resourceObject->getData($pmid);
 	if (($data->getCiteCount()>0) AND (is_int($data->getCiteCount()))) {
 	 array_push($datapoints, $data->getCiteCount());
 	if ($data->getCiteCount() > $max) $max = $data->getCiteCount();
 	 array_push($datanames, $resource." (".$data->getCiteCount().")");
 	}
-     print "<td>".$data->getCiteCount()."</td>";    	
+     print "<td>".$data->getCiteCount()."</td>";    	}
 }
 
 print "</tr></table>";
-$urlimage = "http://chart.apis.google.com/chart?cht=p&chs=512x214&chds=0,$max&chtt=Citation+distribution&chd=t:".implode(",", $datapoints)."&chl=".implode("|",$datanames);
+if ($max >100) $range = "&chds=0,$max";
+$urlimage = "http://chart.apis.google.com/chart?cht=p&chs=512x214$range&chtt=Citation+distribution&chd=t:".trim(implode(",", $datapoints))."&chl=".implode("|",$datanames);
 print "<img src = \"$urlimage\">";
 print $urlimage;
 ?>
