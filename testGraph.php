593cba562f186f1082645d16f0d68737b742d3af
andra.waagmeester@gmail.com <andra.waagmeester@gmail.com@781e20b4-2c82-11df-bd14-c1e331e6ceac>
2010-05-30 15:06:12 +0000

    minor update
    git-svn-id: http://citedin.googlecode.com/svn/trunk@121 781e20b4-2c82-11df-bd14-c1e331e6ceac

--- testGraph.php
+++ testGraph.php
@@ -9,11 +9,26 @@ $datapoints = array();
 $datanames = array();
 $max=0;
 $noResources = 0;
+$wikiIndex =0;
 foreach ($citedin_resources as $resource) {
 	$resourceObject = ResourceRegistry::get($resource);
 	$data = $resourceObject->getData($pmid);
 	if (($data->getCiteCount()>0) AND (is_int($data->getCiteCount()))) {
 		$noResources++;
+		switch ($resourceObject->getResourceType()){
+			case "Wiki":
+				$wikiIndex++;
+				break;
+			case "Database":
+				$databaseIndex++;
+				break;
+			case "API":
+				$apiIndex++;
+				break;
+			case "Published data":
+				$suplmIndex++;
+				break;
+		}
 	 array_push($datapoints, $data->getCiteCount());
 	if ($data->getCiteCount() > $max) $max = $data->getCiteCount();
 	 array_push($datanames, $resource." (".$data->getCiteCount().")");
@@ -25,5 +40,9 @@ if ($max >100) $range = "&chds=0,$max";
 $urlimage = "http://chart.apis.google.com/chart?cht=p&chs=512x214$range&chtt=Citation+distribution&chd=t:".trim(implode(",", $datapoints))."&chl=".implode("|",$datanames);
 print "<img src = \"$urlimage\">";
 print "<hr>";
-print "Citation index: $noResources";
+print "OPD index: $noResources<br>";
+print "Wiki index:$wikiIndex<br>";
+print "Database index: $databaseIndex<br>";
+print "Webservice index: $apiIndex<br>";
+print "Supplementary data index: $suplmIndex";
 ?>
