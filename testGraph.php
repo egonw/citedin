857f3a74fd1643689e96aa2d36556631250b296a
andra.waagmeester@gmail.com <andra.waagmeester@gmail.com@781e20b4-2c82-11df-bd14-c1e331e6ceac>
2010-05-30 14:54:42 +0000

    minor update
    git-svn-id: http://citedin.googlecode.com/svn/trunk@120 781e20b4-2c82-11df-bd14-c1e331e6ceac

--- testGraph.php
+++ testGraph.php
@@ -8,10 +8,12 @@ $pmid = $_GET["pmid"];
 $datapoints = array();
 $datanames = array();
 $max=0;
+$noResources = 0;
 foreach ($citedin_resources as $resource) {
 	$resourceObject = ResourceRegistry::get($resource);
 	$data = $resourceObject->getData($pmid);
 	if (($data->getCiteCount()>0) AND (is_int($data->getCiteCount()))) {
+		$noResources++;
 	 array_push($datapoints, $data->getCiteCount());
 	if ($data->getCiteCount() > $max) $max = $data->getCiteCount();
 	 array_push($datanames, $resource." (".$data->getCiteCount().")");
@@ -22,4 +24,6 @@ foreach ($citedin_resources as $resource) {
 if ($max >100) $range = "&chds=0,$max";
 $urlimage = "http://chart.apis.google.com/chart?cht=p&chs=512x214$range&chtt=Citation+distribution&chd=t:".trim(implode(",", $datapoints))."&chl=".implode("|",$datanames);
 print "<img src = \"$urlimage\">";
+print "<hr>";
+print "Citation index: $noResources";
 ?>
