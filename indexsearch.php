9b865a1289df0d4ac5a820741288e0dd45e28fc6
andra.waagmeester@gmail.com <andra.waagmeester@gmail.com@781e20b4-2c82-11df-bd14-c1e331e6ceac>
2010-06-08 13:42:34 +0000

    aggregated function
    git-svn-id: http://citedin.googlecode.com/svn/trunk@147 781e20b4-2c82-11df-bd14-c1e331e6ceac

--- indexsearch.php
+++ indexsearch.php
@@ -5,13 +5,19 @@
    if (isset($_GET["pubmed_query"])){
 	$query = preg_replace('/\s/', '+', $_GET["pubmed_query"]);
 	$queryResults = simplexml_load_string(file_get_contents("http://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi?db=pubmed&term=$query&retmax=30&usehistory=y&tool=citedin&email=andra.waagmeester@bigcat.unimaas.nl"));
-	//print_r($queryResults);
+
 	$webenv = $queryResults->WebEnv;
 	$querykey = $queryResults->QueryKey;
+	//
+	$pmids = array();
+	foreach ($queryResults->IdList->Id as $id) array_push($pmids, $id);
 	print "<hr>";
+	print "<a href=\"loadBiblioDataMetrics.php?pmids=".implode(",", $pmids)."\" target=\"_blank\">calculate metrics</a>(Slow!)<hr>";
 	
 	$pubmed_xml= new DOMDocument;
  $pubmed_xml->loadXML(file_get_contents("http://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?db=pubmed&WebEnv=$webenv&mode=xml&query_key=$querykey&tool=citedin&email=andra.waagmeester@bigcat.unimaas.nl"));
+	  
+	   
 	  $pubmed_xsl = new DOMDocument;
 	  $pubmed_xsl->load('resources/pubmedSubmit.xsl');
 
