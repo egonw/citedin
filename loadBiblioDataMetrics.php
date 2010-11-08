3646eae0cc14a774e0f42f07f58b513fdaaf55d7
andra.waagmeester@gmail.com <andra.waagmeester@gmail.com@781e20b4-2c82-11df-bd14-c1e331e6ceac>
2010-11-08 14:01:49 +0000

    Introduce term InCiI
    git-svn-id: http://citedin.googlecode.com/svn/trunk@153 781e20b4-2c82-11df-bd14-c1e331e6ceac

--- loadBiblioDataMetrics.php
+++ loadBiblioDataMetrics.php
@@ -4,9 +4,14 @@
         <script type="text/javascript" src="js/jquery-1.4.2.min.js"></script>
 		</head>
 		<body>
-
+<h2>Internet Citation Index (InCiI)</h2>
+The Internet Citation Index or InCiI is a vector containing the number of resources citing a publication and for a each resource the number of times one or a set of publications is cited in the specific resource. 
+<p>
+Since the InCiI is now calculated in a federated search procedure, which means that each resource is checked for the latest version. This could lead to long respons times, we are working on an optimization to increase response times.  
 
 <?php
+
+
 $pmids=$_GET["pmids"];
 print "<script type=\"text/javascript\">
   		$(document).ready(function(){\n
