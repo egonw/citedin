dd5fe1d1113ccadc8a59685b7a8d27fe083705a6
andra.waagmeester@gmail.com <andra.waagmeester@gmail.com@781e20b4-2c82-11df-bd14-c1e331e6ceac>
2010-11-08 16:40:41 +0000

    Added InCiI Cache
    git-svn-id: http://citedin.googlecode.com/svn/trunk@156 781e20b4-2c82-11df-bd14-c1e331e6ceac

--- loadBiblioDataMetrics.php
+++ loadBiblioDataMetrics.php
@@ -24,7 +24,7 @@ if ($notInCache){
 	print "The requested InCiI score is not in our cache, which means that it either not yet calculated, or the last calculation is out-of-date. The requested score is now being calculated in a federated search procedure, which means that each resource is checked for the latest version. This could lead to long respons times, we are working on an optimization to increase response times.";
 foreach ($pmids as $pmid){
 	$mysqldate = date ("Y-m-d");
-	$sqlUpdate = "INSERT INTO InCiIUpdate (pmid, UpdateDate) VALUES ($pmid, $mysqldate);"
+	$sqlUpdate = "INSERT INTO InCiIUpdate (pmid, UpdateDate) VALUES ($pmid, $mysqldate);";
 	mysql_query($sqlUpdate);
 	}	
 	
