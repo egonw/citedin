c7e7b9c46ab6f7b2b67a07d8bbfb1c458acdeee5
andra.waagmeester@gmail.com <andra.waagmeester@gmail.com@781e20b4-2c82-11df-bd14-c1e331e6ceac>
2010-11-08 17:29:18 +0000

    Added InCiI Cache
    git-svn-id: http://citedin.googlecode.com/svn/trunk@164 781e20b4-2c82-11df-bd14-c1e331e6ceac

--- UpdateIncii.php
+++ UpdateIncii.php
@@ -1,10 +1,10 @@
 
 <?
 include 'resources/connectdb.inc';
-print_r($_POST);
 $pmids = explode(",", $_POST["pmids"]);
 print "Your requested InCiI-scoure is currently being calculated. It will appear once it is calculated. The result can also be sent to you: 
-<FORM action = \"SendMail.php\" method =\"post\">
+<FORM action = \"RequestMail.php\" method =\"post\">
+<input name=\"pmids\" type=\"hidden\" value=\"".$_POST["pmids"]."\">
 <input name=\"Email\" id=\"Email\" type=\"text\" size=\"75\"/><input type=\"submit\"></form>";
 
 
