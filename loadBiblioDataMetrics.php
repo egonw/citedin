946ac5354331152a0ab86b36b8d4163fa174cd7d
andra.waagmeester@gmail.com <andra.waagmeester@gmail.com@781e20b4-2c82-11df-bd14-c1e331e6ceac>
2010-11-08 16:39:33 +0000

    Added InCiI Cache
    git-svn-id: http://citedin.googlecode.com/svn/trunk@155 781e20b4-2c82-11df-bd14-c1e331e6ceac

--- loadBiblioDataMetrics.php
+++ loadBiblioDataMetrics.php
@@ -7,10 +7,28 @@
 <h2>Internet Citation Index (InCiI)</h2>
 The Internet Citation Index or InCiI is a vector containing the number of resources citing a publication and for a each resource the number of times one or a set of publications is cited in the specific resource. 
 <p>
-Since the InCiI is now calculated in a federated search procedure, which means that each resource is checked for the latest version. This could lead to long respons times, we are working on an optimization to increase response times.  
+  
 
 <?php
+include 'resources/connectdb.inc';
+$pmids = explode(",", $_GET["pmids"]);
+foreach ($pmids as $pmid){
+    $result = mysql_query("SELECT * from InCiIUpdate where pmid=$pmid");
+    if (mysql_num_rows($result)== 0){
+	   $notInCache = TRUE;
+	   break;
+    }
+}
 
+if ($notInCache){
+	print "The requested InCiI score is not in our cache, which means that it either not yet calculated, or the last calculation is out-of-date. The requested score is now being calculated in a federated search procedure, which means that each resource is checked for the latest version. This could lead to long respons times, we are working on an optimization to increase response times.";
+foreach ($pmids as $pmid){
+	$mysqldate = date ("Y-m-d");
+	$sqlUpdate = "INSERT INTO InCiIUpdate (pmid, UpdateDate) VALUES ($pmid, $mysqldate);"
+	mysql_query($sqlUpdate);
+	}	
+	
+}
 
 $pmids=$_GET["pmids"];
 print "<script type=\"text/javascript\">
