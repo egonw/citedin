0e8166c076b355c9e4d9dcd4c6d120ffbf4a99f5
andra.waagmeester@gmail.com <andra.waagmeester@gmail.com@781e20b4-2c82-11df-bd14-c1e331e6ceac>
2010-11-09 16:37:24 +0000

    Improving InCiI Cache
    git-svn-id: http://citedin.googlecode.com/svn/trunk@183 781e20b4-2c82-11df-bd14-c1e331e6ceac

--- UpdateIncii.php
+++ UpdateIncii.php
@@ -4,16 +4,13 @@ include 'resources/connectdb.inc';
 
 
 $pmids = explode(",", $_POST["pmids"]);
+putenv("pmids=".$_POST["pmids"]);
 print "Your requested InCiI-scoure is currently being calculated. It will appear once it is calculated. The result can also be sent to you: 
 <FORM action = \"RequestMail.php\" method =\"post\">
 <input name=\"pmids\" type=\"hidden\" value=\"".$_POST["pmids"]."\">
 <input name=\"Email\" id=\"Email\" type=\"text\" size=\"75\"/><input type=\"submit\"></form><hr>";
 
-$descriptorspec = array(
-   0 => array("pipe", "r"),  // stdin is a pipe that the child will read from
-   1 => array("pipe", "w"),  // stdout is a pipe that the child will write to
-   2 => array("file", "/tmp/error-output.txt", "a") // stderr is a file to write to
-);
+exec("/usr/bin/php RunInCiIUpdate.php >/dev/null &");
 
 $cwd = '/tmp';
 $env = array('pmids' => $_POST["pmids"]);
@@ -26,10 +23,9 @@ if (is_resource($process)) {
     // Any error output will be appended to /tmp/error-output.txt
 
     fwrite($pipes[0], "RunInCiIUpdate.php");
-    fclose($pipes[0]);
+    
 
-    echo stream_get_contents($pipes[1]);
-    fclose($pipes[1]);
+    
 
     // It is important that you close any pipes before calling
     // proc_close in order to avoid a deadlock
