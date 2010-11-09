53e41ae79673bffc6eff23164315214672593427
andra.waagmeester@gmail.com <andra.waagmeester@gmail.com@781e20b4-2c82-11df-bd14-c1e331e6ceac>
2010-11-09 13:24:51 +0000

    Added InCiI Cache
    git-svn-id: http://citedin.googlecode.com/svn/trunk@165 781e20b4-2c82-11df-bd14-c1e331e6ceac

--- UpdateIncii.php
+++ UpdateIncii.php
@@ -1,6 +1,12 @@
 
 <?
 include 'resources/connectdb.inc';
+
+require_once("resources/ResourceRegistry.php");
+set_time_limit(0);
+ResourceRegistry::init();
+$citedin_resources = ResourceRegistry::listResources();
+
 $pmids = explode(",", $_POST["pmids"]);
 print "Your requested InCiI-scoure is currently being calculated. It will appear once it is calculated. The result can also be sent to you: 
 <FORM action = \"RequestMail.php\" method =\"post\">
@@ -11,6 +17,14 @@ print "Your requested InCiI-scoure is currently being calculated. It will appear
 foreach ($pmids as $pmid){
 	$sqlUpdate = "INSERT INTO InCiIUpdate (pmid, UpdateDate) VALUES ($pmid, CURDATE());";
 	mysql_query($sqlUpdate);
-	
+	$lastUpdateId = mysql_insert_id();
+    foreach ($citedin_resources as $resource){
+		$resourceInfo = ResourceRegistry::get($resource);
+		$resourceName = $resourceInfo->getResourceName();
+		$resourceData = $resourceInfo->getData($pmid);
+		$resourceCount = $resourceData->getCiteCount();
+		$sqlResource = "INSERT INTO InCiIResource (IUId, Resource, freq) VALUES ($lastUpdateId, $resourceName, $resourceCount);";
+		mysql_query($sqlResource);
 	}
+}
 ?>
