878f07525307a09906130cb95c13ad5ad1764624
andra.waagmeester@gmail.com <andra.waagmeester@gmail.com@781e20b4-2c82-11df-bd14-c1e331e6ceac>
2010-11-09 16:38:13 +0000

    Improving InCiI Cache
    git-svn-id: http://citedin.googlecode.com/svn/trunk@184 781e20b4-2c82-11df-bd14-c1e331e6ceac

--- UpdateIncii.php
+++ UpdateIncii.php
@@ -12,28 +12,5 @@ print "Your requested InCiI-scoure is currently being calculated. It will appear
 
 exec("/usr/bin/php RunInCiIUpdate.php >/dev/null &");
 
-$cwd = '/tmp';
-$env = array('pmids' => $_POST["pmids"]);
-$process = proc_open('php', $descriptorspec, $pipes, $cwd, $env);
-
-if (is_resource($process)) {
-    // $pipes now looks like this:
-    // 0 => writeable handle connected to child stdin
-    // 1 => readable handle connected to child stdout
-    // Any error output will be appended to /tmp/error-output.txt
-
-    fwrite($pipes[0], "RunInCiIUpdate.php");
-    
-
-    
-
-    // It is important that you close any pipes before calling
-    // proc_close in order to avoid a deadlock
-   // $return_value = proc_close($process);
-
-    //echo "command returned $return_value\n";
-}
-
-
 
 ?>
