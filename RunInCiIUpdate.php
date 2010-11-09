4f79d95e6a083b3ad97703694025186b99669730
andra.waagmeester@gmail.com <andra.waagmeester@gmail.com@781e20b4-2c82-11df-bd14-c1e331e6ceac>
2010-11-09 16:22:54 +0000

    Improving InCiI Cache
    git-svn-id: http://citedin.googlecode.com/svn/trunk@180 781e20b4-2c82-11df-bd14-c1e331e6ceac

--- RunInCiIUpdate.php
+++ RunInCiIUpdate.php
@@ -6,7 +6,7 @@ set_time_limit(0);
 ResourceRegistry::init();
 $citedin_resources = ResourceRegistry::listResources();
 $pmids = explode(",", $_ENV["pmids"]);
-
+$pmids = array("1234567", "7654321");
 foreach ($pmids as $pmid){
 	$sqlUpdate = "INSERT INTO InCiIUpdate (pmid, UpdateDate) VALUES ($pmid, CURDATE());";
 	mysql_query($sqlUpdate);
