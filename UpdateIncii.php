ef8f7fe65b674e2ee5e88c9b974f12ffef75c398
andra.waagmeester@gmail.com <andra.waagmeester@gmail.com@781e20b4-2c82-11df-bd14-c1e331e6ceac>
2010-11-10 09:57:39 +0000

    Improving InCiI Cache
    git-svn-id: http://citedin.googlecode.com/svn/trunk@200 781e20b4-2c82-11df-bd14-c1e331e6ceac

--- UpdateIncii.php
+++ UpdateIncii.php
@@ -11,6 +11,10 @@ print "Your requested InCiI-scoure is currently being calculated. It will appear
 <input name=\"Email\" id=\"Email\" type=\"text\" size=\"75\"/><input type=\"submit\"></form><hr>";
 
 exec("/usr/bin/php RunInCiIUpdate.php ".$_POST["pmids"]." >/dev/null &");
+$incache = $_POST["incache"];
+$notincache = $_POST["notincache"];
+$url = "http://chart.apis.google.com/chart?cht=p3&chs=320x100&chd=t:$incache,$notincache&chl=Cache|Not+in+cache";
+print "<IMG SRC=\"$url\">";
 
 
 ?>
--- loadBiblioDataMetrics.php
+++ loadBiblioDataMetrics.php
@@ -24,12 +24,12 @@ if ($incache > $notincache){
 
 
 if ($notallInCache){
-	$url = "http://chart.apis.google.com/chart?cht=p3&chs=320x100&chd=t:$incache,$notincache&chl=Cache|Not+in+cache";
-	print $url;
-	print "<IMG SRC=\"$url\">";
+
 	print "The requested InCiI score is not in our cache, which means that it either not yet calculated, or the last calculation is out-of-date. You can initiate a InCiI score calculation by completing the following form. Calculating a InCiI-score requires a federated search procedure. Different online resource are consulted. To prevent overloading these resource, we actively dose the requests. If you like to calculate the InCiI-score, you can initiate a calculation by filling the following form. An email will be sent, once the InCiI score is updated. <hr>
 	<FORM action = \"UpdateIncii.php\" method =\"post\">
 	<input name=\"pmids\" type=\"hidden\" value=\"".$_GET["pmids"]."\">
+	<input name=\"incache\" type=\"hidden\" value=\"$incache\">
+	<input name=\"notincache\" type=\"hidden\" value=\"$notincache\">
 	<input type=\"submit\" value=\"Start InCiI-score calculation\"></form>";
 }	
 	
