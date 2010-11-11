02eb7b5fa58efed69ff2c52cfe28a87b3dc12738
andra.waagmeester@gmail.com <andra.waagmeester@gmail.com@781e20b4-2c82-11df-bd14-c1e331e6ceac>
2010-11-11 18:14:33 +0000

    better twitter integration
    git-svn-id: http://citedin.googlecode.com/svn/trunk@231 781e20b4-2c82-11df-bd14-c1e331e6ceac

--- SubmitTwitter.php
+++
@@ -1,8 +0,0 @@
-<?php
-require 'tweet.php';
-
-$tweet = $_GET["tweet"];
-$retarr = post_tweet(OAUTH_CONSUMER_KEY, OAUTH_CONSUMER_SECRET,
-                           $tweet, $access_token, $access_token_secret,
-                           true, true);
-?>
