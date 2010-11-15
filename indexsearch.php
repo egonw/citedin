8ec78cb9524866195c5709bc3e3522694e01cab7
andra.waagmeester@gmail.com <andra.waagmeester@gmail.com@781e20b4-2c82-11df-bd14-c1e331e6ceac>
2010-11-15 20:09:32 +0000

    minor update
    git-svn-id: http://citedin.googlecode.com/svn/trunk@257 781e20b4-2c82-11df-bd14-c1e331e6ceac

--- indexsearch.php
+++ indexsearch.php
@@ -13,7 +13,13 @@
 	foreach ($queryResults->IdList->Id as $id) array_push($pmids, $id);
 	print "<hr>";
 	
-    if ($_GET["callscript"]=="citedin")	print "<a href=\"loadBiblioDataMetrics.php?pmids=".implode(",", $pmids)."\" target=\"_blank\">calculate CitedIn Internet Citation Score</a><hr>";
+  //  if ($_GET["callscript"]=="citedin")	print "<a href=\"loadBiblioDataMetrics.php?pmids=".implode(",", $pmids)."\" target=\"_blank\">calculate CitedIn Internet Citation Score</a><hr>";
+
+if ($_GET["callscript"]=="citedin") print "<form action=\"loadBiblioDataMetrics.php\" method=\"get\" accept-charset=\"utf-8\">
+	<input type=\"hidden/\" name=\"pmids\" value=\"".implode(",", $pmids)."\" id=\"pmids\">
+
+	<p><input type=\"submit\" value=\"calculate CitedIn Internet Citation Score<\"></p>
+</form>";
 	
 	$pubmed_xml= new DOMDocument;
  $pubmed_xml->loadXML(file_get_contents("http://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?db=pubmed&WebEnv=$webenv&mode=xml&query_key=$querykey&tool=citedin&email=andra.waagmeester@bigcat.unimaas.nl"));
