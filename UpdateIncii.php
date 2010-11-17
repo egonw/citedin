2e5a889db09c3d4f827f583b3e18ae9e9160d7f7
andra.waagmeester@gmail.com <andra.waagmeester@gmail.com@781e20b4-2c82-11df-bd14-c1e331e6ceac>
2010-11-17 22:48:45 +0000

    minor update
    git-svn-id: http://citedin.googlecode.com/svn/trunk@290 781e20b4-2c82-11df-bd14-c1e331e6ceac

--- UpdateIncii.php
+++ UpdateIncii.php
@@ -5,12 +5,7 @@ include 'resources/connectdb.inc';
 
 $pmids = explode(",", $_POST["pmids"]);
 
-print "Your requested InCiI-scoure is currently being calculated. It will appear once it is calculated. The result can also be sent to you: 
-<FORM action = \"RequestMail.php\" method =\"post\">
-<input name=\"pmids\" type=\"hidden\" value=\"".$_POST["pmids"]."\">
-Email: <input name=\"Email\" id=\"Email\" type=\"text\" size=\"75\"/><br>
-Twitter: <input name=\"Twitter\" id=\"Twitter\" type=\"text\" size=\"75\"/>
-<input type=\"submit\"></form><hr>";
+print "Your requested InCiI-scoure is currently being calculated. It will appear once it is calculated. The result can also be sent to you:";
 $commandpmids = $_POST["pmids"];
 $bitlyUrl = $_POST["bitlyUrl"];
 $email = $_POST["email"];
