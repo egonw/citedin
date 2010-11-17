26e5a737008c8a0cb238fba1cfaa80e435e67d00
andra.waagmeester@gmail.com <andra.waagmeester@gmail.com@781e20b4-2c82-11df-bd14-c1e331e6ceac>
2010-11-17 22:50:46 +0000

    minor update
    git-svn-id: http://citedin.googlecode.com/svn/trunk@292 781e20b4-2c82-11df-bd14-c1e331e6ceac

--- UpdateIncii.php
+++ UpdateIncii.php
@@ -5,7 +5,7 @@ include 'resources/connectdb.inc';
 
 $pmids = explode(",", $_POST["pmids"]);
 
-print "Your requested InCiI-scoure is currently being calculated. It will appear once it is calculated. The result can also be sent to you:";
+print "Your requested InCiI-scoure is currently being calculated. ";
 $commandpmids = $_POST["pmids"];
 $bitlyUrl = $_POST["bitlyUrl"];
 $email = $_POST["email"];
